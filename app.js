7257bc1c567584969e0d38bcef6433ae7ad39f2e
Vlad Meshcherinov <vladislav1310@gmail.com>
2022-07-26 15:24:25 +0400

    add helmet and rate limiter to app

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const router = require('./routes');
 
 const { PORT = 3000 } = process.env;
 const DB_URL = 'mongodb://localhost:27017/mestodb';
 const app = express();
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
 
 app.use(express.json());
+app.use(helmet());
+app.use(limiter);
+
 mongoose.connect(DB_URL);
 
 app.use((req, res, next) => {
