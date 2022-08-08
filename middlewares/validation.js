288fc3aa8750aae48ecd4b30546ee78f55111281
Vlad Meshcherinov <vladislav1310@gmail.com>
2022-08-08 22:05:08 +0400

    fix url validation bug

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,10 +1,12 @@
 const { celebrate, Joi } = require('celebrate');
 
+const URL_REG_EXP = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)?/;
+
 const signupValidation = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().uri(),
+    avatar: Joi.string().pattern(URL_REG_EXP),
     email: Joi.string()
       .required()
       .min(6)
@@ -34,14 +36,14 @@ const updateUserProfileValidation = celebrate({
 
 const updateUserAvatarValidation = celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string().required().uri(),
+    avatar: Joi.string().required().pattern(URL_REG_EXP),
   }),
 });
 
 const createCardValidation = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().uri(),
+    link: Joi.string().required().pattern(URL_REG_EXP),
   }),
 });
 
--- models/card.js
+++ models/card.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 
 const cardSchema = new mongoose.Schema({
   name: {
@@ -11,11 +10,6 @@ const cardSchema = new mongoose.Schema({
   link: {
     type: String,
     required: true,
-    validate: {
-      validator(link) {
-        return /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)?/.test(link);
-      },
-    },
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
