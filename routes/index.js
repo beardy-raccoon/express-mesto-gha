981b2bc50982c10894dababf6bce1a755b900ed6
Vlad Meshcherinov <vladislav1310@gmail.com>
2022-08-09 03:44:11 +0400

    run tests

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,14 @@ const auth = require('../middlewares/auth');
 const userRouter = require('./users');
 const cardRouter = require('./cards');
 const publicRouter = require('./public');
+const NotFoundError = require('../errors/not-found-error');
 
 router.use(publicRouter);
 router.use(auth, userRouter);
 router.use(auth, cardRouter);
 router.use(errors());
+router.use((req, res, next) => {
+  next(new NotFoundError('Запрашиваемая страница не найдена'));
+});
 
 module.exports = router;
