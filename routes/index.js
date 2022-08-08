81fe85a85733e7a95635fc2689fe782cf26d3ee1
Vlad Meshcherinov <vladislav1310@gmail.com>
2022-08-09 03:34:40 +0400

    add centralized errors handler

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,10 @@ const auth = require('../middlewares/auth');
 const userRouter = require('./users');
 const cardRouter = require('./cards');
 const publicRouter = require('./public');
-const { NOT_FOUND_ERR, NOT_FOUND_URL_ERR_MESSAGE } = require('../errors/errors');
 
 router.use(publicRouter);
 router.use(auth, userRouter);
 router.use(auth, cardRouter);
 router.use(errors());
-router.use((req, res) => {
-  res.status(NOT_FOUND_ERR).send({ message: NOT_FOUND_URL_ERR_MESSAGE });
-});
 
 module.exports = router;
